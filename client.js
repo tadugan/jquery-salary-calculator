8e7e077533368a6078b723f921727bc552fd9af1
Tim Dugan <tadugan@me.com>
2021-06-19 14:08:35 -0500

    updateDom runs when employee is add, adds employee properties to table

--- client.js
+++ client.js
@@ -37,21 +37,58 @@ function addEmployee() {
     let lastNameInput = $('#lastNameInput').val();
     let idInput = $('#idInput').val();
     let titleInput = $('#titleInput').val();
-    let annualSalaryInput = $('#annualSalaryInput').val();
+    let annualSalaryInput = Number($('#salaryInput').val());
+
+    console.log(annualSalaryInput);
+
     // create new employee object
     const newEmployee = new Employee(firstNameInput, lastNameInput, idInput, titleInput, annualSalaryInput);
     // add new employee object to employees array
     employees.push(newEmployee);
     // update table on DOM with info from employees array
     // update total monthly on the DOM
+    updateDom();
     // clear input fields
+
+    // test employees array
     console.log(employees);
 }
 
+// constructor to create new Employee objects
 function Employee(firstName, lastName, id, title, annualSalary) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.id = id;
     this.title = title;
     this.annualSalary = annualSalary;
+}
+
+// updates table and total
+function updateDom() {
+    //target table element
+    let tableEl = $('#employeeTable');
+    // empty table element
+    tableEl.empty();
+    // append header row info
+    tableEl.append(`<tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>ID</th>
+    <th>Title</th>
+    <th>Annual Salary</th>
+    <th>Delete?</th>
+</tr>`)
+    // append info to table
+    for (let employee of employees) {
+        console.log('each employee');
+        tableEl.append(`<tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.id}</td>
+        <td>${employee.title}</td>
+        <td>${employee.annualSalary}</td>
+        <td></td>
+    </tr>`);
+    }
+    // update total monthly cost
 }
